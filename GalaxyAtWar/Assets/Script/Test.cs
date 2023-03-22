2ec950a1e47427b04cca5dac292902edd6bf0bf4
tangyi <1085979333@qq.com>
2023-03-22 13:58:07 +0800

    2.0

--- GalaxyAtWar/Assets/Script/Test.cs
+++ GalaxyAtWar/Assets/Script/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("这是一个测试类");
     }
 
     // Update is called once per frame
